08eb63ed3c68fd7f0bc11f1febc11354fecf9f7d
2xd <73307604+2xddev@users.noreply.github.com>
2026-08-04 04:32:24 +0200

    fix: my life

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequestHandler } from "@react-router/express";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "client", "assets"), {
+    immutable: true,
+    maxAge: "1y",
+  })
+);
+
+app.use(express.static(path.join(__dirname, "client")));
+
+app.all(
+  /.*/,
+  createRequestHandler({
+    build: () => import("./server/index.js"), // Let op: vaak staat dit in de build-map!
+  })
+);
+
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+  console.log(`Server draait op poort ${port}`);
+});
